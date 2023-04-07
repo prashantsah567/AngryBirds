94bcdb95a92b93fcb673f4d318304f638de847ff
Prashant Sah <prashantsah567@gmail.com>
2023-04-07 09:09:31 -0400

    header components page createc

---
+++ src/Components/CreateContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreateContent = () =>{
+
+    return(
+        <div>
+            <p>Create a new content here</p>
+        </div>
+    );
+}
+
+export default CreateContent;
---
+++ src/Components/ViewContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ViewContent = () =>{
+
+    return(
+        <div>
+            <p>View all contents here</p>
+        </div>
+    );
+}
+
+export default ViewContent;
